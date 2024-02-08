b6944b2df142594daf46eb20ca9f418bc03e17ab
Jai_Vang <jaivang0387@gmail.com>
2024-02-08 07:15:10 -0500

    chaning the data.guest_session_id with the ? operator (data?.guest_session_id) to allow user to safely access properties of an object that may be 'null' or 'undefinded' without causing an error. This also enable user to select the 'login' button once instead twice before logging in.

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -13,7 +13,7 @@ export const Auth = () => {
 
     const handleLogin = async () => {
         await mutate();
-        localStorage.setItem("guest_session_id", data.guest_session_id);
+        localStorage.setItem("guest_session_id", data?.guest_session_id);
         navigate("/");
     };
 
